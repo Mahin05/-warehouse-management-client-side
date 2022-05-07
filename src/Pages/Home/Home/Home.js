d84deec7cbfab26f1b7e7db35bd0f0ae7d991ff4
Mahin05 <mahin05@gmail.com>
2022-05-07 23:21:20 +0600

    .gitignore file updated

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from '../../Banner/Banner';
 import GoMobileWithAPP from '../GoMobileWithAPP/GoMobileWithAPP';
 import Inventories from '../Inventories/Inventories';
