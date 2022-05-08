9525e258a6c748f37ab72270777936d4b334df8b
Mahin05 <mahin05@gmail.com>
2022-05-08 16:21:32 +0600

    increase functionalities done

--- src/Pages/inventoryDetail/inventoryDetail.js
+++ src/Pages/inventoryDetail/inventoryDetail.js
@@ -22,19 +22,7 @@ const InventoryDetail = () => {
             .then(res => res.json())
             .then(data => setInventory(data))
     }, [inventoryId])
-    // const handleQntyCnge = event => {
-    //     event.preventDefault()
-    //     console.log(event.target.value)
-    //     const {quantity, ...rest} = inventory;
-    //     const newQnty = parseInt(event.target.value);
-    //     const addQnty =  parseInt(inventory.quantity) + newQnty;
-    //     const updtQnty = {quantity:addQnty, ...rest};
-    //     console.log(quantity,rest);
-    //     console.log(updtQnty);
-    //     setInventory(updtQnty);
 
-
-    // }
     const handleDelivered = event => {
         event.preventDefault()
         console.log(event.target.value)
@@ -66,85 +54,33 @@ const InventoryDetail = () => {
             alert('Stocked out!!')
         }
     }
-    // const handleDelivery = event => {
-    //     event.preventDefault()
-    //     const quantity = event.target.quantity.value;
-    //     const updatedQuantity = {quantity}
-    //     // send data 
-    //     const url = `http://localhost:5000/inventory/${inventoryId}`
-    //     fetch(url,{
-    //         method: 'PUT',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(updatedQuantity)
-    //     })
-    //         .then(res => res.json())
-    //         .then(result => {
-    //             alert('Quantity Added Successfully!!')
-    //             console.log(result);
-    //             event.target.reset();
-    //         })
-    // }
+    const handleRestock = event => {
+        event.preventDefault()
+        console.log(event.target.value)
+        const { quantity, ...rest } = inventory;
+        const newQuantity = parseInt(event.target.quantity.value);
+        const addedQuantity = parseInt(inventory.quantity) + newQuantity;
+        const updatedQuantity = { quantity: addedQuantity, ...rest };
+        console.log(quantity, rest);
+        console.log(updatedQuantity);
+        setInventory(updatedQuantity);
+        // send data 
+        const url = `http://localhost:5000/inventory/${inventoryId}`
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedQuantity)
+        })
+            .then(res => res.json())
+            .then(result => {
+                alert('Quantity Added Successfully!!')
+                console.log(result);
+                event.target.reset();
+            })
 
-    // const handleUpdateStock = event => {
-    //     event.preventDefault()
-    //     const quantity = event.target.quantity.value;
-    //     // send data to server
-    //     fetch('http://localhost:5000/inventory',{
-    //         method: 'POST',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(data)
-    //     })
-    //         .then(res => res.json())
-    //         .then(result => {
-    //             console.log(result);
-    //         })
-    // }
-    //     const handleDelivery = event => {
-    //         event.preventDefault();
-    //         const deliverey = {
-    //             email: user.email,
-    //             inventory: inventory.name,
-    //             img:inventory.img,
-    //             description:inventory.description,
-    //             price:inventory.price,
-    //             // quantity:inventory.quantity,
-    //             // newQuantity : inventory-1,
-    //             supplierName:inventory.supplierName,
-    //             inventoryId: inventoryId,
-    //             // quantity: event.target.quantity.value,
-    //             phone: event.target.phone.value
-
-    //         }
-    //         // Send a POST request
-    //         axios.post('https://nameless-mesa-10052.herokuapp.com/inventory',deliverey)
-    //         .then(res => {
-    //             // console.log(res)
-    //             const {data} =res;
-    //             if(data.insertedId){
-    //                 toast('Your item is booked!!');
-    //                 event.target.reset();
-    //             }
-    //         })
-    //     }
-    //     const handleRestock = event => {
-    //         const deliverey = {
-    //             quantity: event.target.quantity.value
-    //         }
-    //         // Send a POST request
-    //         axios.post('https://nameless-mesa-10052.herokuapp.com/inventory',deliverey)
-    //         .then(res => {
-    //             // console.log(res)
-    //             const {data} =res;
-    //             if(data.insertedId){
-    //                 toast('Restocked!!');
-    //                 event.target.reset();
-    //             }
-    //         })
-    // }
+    }
 
     return (
         <div className='w-50 mx-auto'>
@@ -170,7 +106,7 @@ const InventoryDetail = () => {
                 {/* <input className='mb-2' value={inventory.quantity} placeholder='quantity' type="number" {...register("quantity")} required readOnly /> */}
                 <input className='btn btn-primary' type="submit" value="Delivered" />
             </form>
-            <form>
+            <form onSubmit={handleRestock}>
                 <h2>Restock the item</h2>
                 <input className='w-100 mb-2' type="text" name="quantity" placeholder='quantity' required /> <br />
                 <input type="submit" value="Restock" />
